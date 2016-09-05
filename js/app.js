096538f296b682c16dbaec5ba2bc97e81705da76
Otniel <otniel.a.55@gmail.com>
2016-09-05 15:37:21 -0400

    buenas practicas app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
-(function($, window){
+/** codeAwesome.com.ve scripts by codeAwesome
+*	v1.0
+*/
+
+(function ($, window) {
+	'use strict';
+
+/*---- global variables & constants ----*/
+	var FORM_CONTROL = '.form-control',
+		BTN_SEND = '#send',
+		REGEX_NAME = /^[a-zA-Z0-9 ñáéíóú]*$/,
+		REGEX_MAIL = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+		FORM_XS = '#contact-form',
+		FORM_FOCUSOUT = "contact-form",
+		FORM_FOCUSIN = "contact-form-focus",
+		FORM_HIDE = "hidden",
+		$htmlBody = $('html, body');
 
 /*---- document ready & window resize ----*/
 
-	$(document).ready(function(){
+	$(document).ready(function () {
 
 		setTimeout(activeAnimations, 2000);
 		contactUsInSm();
@@ -10,14 +26,14 @@
 
 	});
 
-	$(window).resize(function(){
+	$(window).resize(function () {
 
 		contactUsInSm();
 		viewXs();
 
 	});
     
-/*---------- header controllers ----------*/
+/*---------- header controller ----------*/
 
 	/*-- camera instance & config --*/
 	$('.camera_wrap').camera({
@@ -27,74 +43,74 @@
 	});
 
 	/*-- remove the focus to the tags "a" --*/
-	$('.nav-pills > li > a').focus(function(){ $(this).blur(); });
+	$('.nav-pills > li > a').focus(function () { $(this).blur(); });
 
 	/*-- slide scroll to href --*/
 	$('a[href*="#"]').click(slideScrollAnimation);
 
 
-/*--------- services controllers ---------*/
+/*--------- services controller ---------*/
 
 	/*-- flipCarousel instance & config --*/
 	$('.flip-img').flipcarousel();
 
-/*--------- contactUS controllers --------*/
+/*--------- contactUS controller --------*/
 
 	/*-- disabled #send button --*/
-	$('#send').prop('disabled', true);
+	$(BTN_SEND).prop('disabled', true);
 
 	/* send mail onclick event */
-	$('#send').click(sendEmail);
+	$(BTN_SEND).click(sendEmail);
 
 	/*-- input animation onfocusIn event --*/
-	$('.form-control').focusin(function(){
+	$(FORM_CONTROL).focusin(function () {
+		var $this = $(this);
 
-		$(this).addClass("active");
-		$(this).siblings('label').addClass("active");
+		$this.addClass("active");
+		$this.siblings('label').addClass("active");
 	});
 
 	/*-- input animation onfocusOut event --*/
-	$('.form-control').focusout(function(){
+	$(FORM_CONTROL).focusout(function () {
+		var $this = $(this);
 
-		var input = $(this);
-
-		if( !input.val().trim() ){
-			input.removeClass("active");
-			input.siblings('label').removeClass("active");
+		if (!$this.val().trim()) {
+			$this.removeClass("active");
+			$this.siblings('label').removeClass("active");
 		}
 	});
 
 	/*-- input validation onkeyUp event --*/
-	$('.form-control').on('keyup change', validateInput);
+	$(FORM_CONTROL).on('keyup change', validateInput);
 
-	/*-- button.backTop onclick event --*/
-	$('.backTop').click(function(){ $('html, body').animate({ scrollTop: 0 }); });
+/*----- button.backTop onclick event -----*/
+	$('.backTop').click(function () { $htmlBody.animate({ scrollTop: 0 }); });
 
 /*-------------- functions ---------------*/
 
 	function slideScrollAnimation() {
 
-		if ( location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "")
-			&& location.hostname == this.hostname ) {
+		if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") &&
+			location.hostname === this.hostname) {
 
-			var $target = $(this.hash);
+			var $target = $(this.hash),
+				targetOffset = "";
 
-			$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+			$target = ($target.length && $target) || $('[name=' + this.hash.slice(1) + ']');
 
 			if ($target.length) {
 
-				var targetOffset = $target.offset().top;
+				targetOffset = $target.offset().top;
 
-				$('html, body').animate({scrollTop: targetOffset}, 500);
+				$htmlBody.animate({scrollTop: targetOffset}, 1000);
 
 				return false;
-
 			}
 		}
 	}
 
-	function animationIn(element, animation, offset){
-        $(element).waypoint(function(){
+	function animationIn(element, animation, offset) {
+        $(element).waypoint(function () {
             $(element).toggleClass("animated " + animation);
             this.destroy();
         }, {
@@ -103,26 +119,26 @@
         });
     }
 
-	function activeAnimations(){
+	function activeAnimations() {
 
 		/*-- backTop --*/
-		$('header .main-container').waypoint(function(direction){
-			if(direction == "down"){
+		$('header .main-container').waypoint(function (direction) {
+			if (direction === "down") {
 				$('.backTop').removeClass("fadeOutDown");
 				$('.backTop').addClass("animated").addClass("fadeInUp");
 			} else {
 				$('.backTop').removeClass("fadeInUp").removeClass("slideInDown");
 				$('.backTop').addClass("animated").addClass("fadeOutDown");
 			}
-		},{
-			offset:function(){
+		}, {
+			offset: function () {
 				return -$('header .main-container').height();
 			}
 		});
 
-		$('footer:eq(0)').waypoint(function(direction){
-			if(direction == "down"){
-				$('.backTop').css('bottom', $('footer').outerHeight()+5);
+		$('footer:eq(0)').waypoint(function (direction) {
+			if (direction === "down") {
+				$('.backTop').css('bottom', $('footer').outerHeight() + 5);
                 $('.backTop').removeClass("slideInDown").removeClass("fadeInUp");
                 $('.backTop').addClass("slideInUp");
 			} else {
@@ -130,8 +146,8 @@
 				$('.backTop').removeClass("slideInUp");
                 $('.backTop').addClass("slideInDown");
 			}
-		},{
-			offset:'95%'
+		}, {
+			offset: '95%'
 		});
 
 		/* whoAreWe */
@@ -156,109 +172,92 @@
 		animationIn('.contact-method:eq(1)', "rollIn", '80%');
 	}
 
-	function formValid($content){
-		var $valid = true;
+	function formValid(form) {
+		var valid = true;
 
-		$('.form-control', $content).each(function(){
-			$valid = $valid && $(this).hasClass("valid");
-		})
+		$(FORM_CONTROL, form).each(function () {
+			valid = valid && $(this).hasClass("valid");
+		});
 
-		return $valid;
+		return valid;
 	}
 
-	function validateInput(nodo){
-		var email = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		var name = /^[a-zA-Z0-9 ñáéíóú]*$/;
-		var nodo = nodo instanceof jQuery ? nodo : $(this);
-		var label = nodo.siblings('label');
-		var $form = nodo[0].form;
+	function validateInput($nodo) {
+		$nodo = $nodo instanceof jQuery ? $nodo : $(this);
+		var $label = $nodo.siblings('label'),
+			form = $nodo[0].form,
+			value = $nodo.val();
 
-		nodo.removeClass("error valid");
-		label.removeClass("empty invalid valid");
+		$nodo.removeClass("error valid");
+		$label.removeClass("invalid valid");
 
-		if( !nodo.val().trim() ){
+		if (!$.trim(value) || ($nodo.is(':text') && !REGEX_NAME.test(value)) ||
+			($nodo.attr('type') === "email" && !REGEX_MAIL.test(value))) {
 
-			nodo.addClass("error");
-			label.addClass("empty");
-			$('#send', $form).prop('disabled', true);
+			$nodo.addClass("error");
+			$label.addClass("invalid");
+			$(BTN_SEND, form).prop('disabled', true);
 
 			return false;
-
-		} else {
-			if ( nodo.is(':text') && !name.test(nodo.val()) ){
-				nodo.addClass("error");
-				label.addClass("empty");
-
-				return false;
-			}
-
-			if( nodo.attr('type') == "email" && !email.test(nodo.val()) ){
-
-				nodo.addClass("error");
-				label.addClass("invalid");
-				$('#send', $form).prop('disabled', true);
-
-				return false;
-			}
 		}
 
-		nodo.addClass("valid");
-		label.addClass("valid");
+		$nodo.addClass("valid");
+		$label.addClass("valid");
 
-		if( formValid($form) ) $('#send', $form).prop('disabled', false);
+		if (formValid(form))
+			$(BTN_SEND, form).prop('disabled', false);
 
 		return true;
 	}
 
-	function sendEmail(){
+	function sendEmail() {
+		var valid = true,
+			form = $(this)[0].form,
+			$elements = $(FORM_CONTROL, form);
 
-		var $valid = true;
-		var $form = $(this)[0].form;
-		var inputs = $('.form-control', $form);
-
-		inputs.each(function(k, nodo){
-			$valid = $valid && validateInput($(nodo));
+		$elements.each(function (k, nodo) {
+			valid = valid && validateInput($(nodo));
 			$(nodo).blur();
 		});
 
-		if( $valid && formValid($form) ){
+		if (valid && formValid(form)) {
 
 			$.ajax({
 				url: "https://formspree.io/codeawesomepro@gmail.com",
 				method: "POST",
 				data: {
-					message: $("#message").val(),
-					_replyto: $("#correo").val(),
-					name: $("#name").val(),
+					message: $elements.eq(2).val(),
+					_replyto: $elements.eq(1).val(),
+					name: $elements.eq(0).val(),
 					_subject: "correo de usuario de codeawesome.com.ve"
 				},
 				dataType: "json",
-				beforeSend:function(){
+				beforeSend: function () {
 					swal({
-						title:"Enviando...",
-						text:"Espere mientras se envia su mensaje",
+						title: "Enviando...",
+						text: "Espere mientras se envia su mensaje",
 						imageUrl: "pic/cargando.gif",
 						showConfirmButton: false
 					});
 				},
-				success: function(){
+				success: function () {
 					swal({
-						title:"Excelente!",
-						text:"Su correo ha sido enviado con éxito, nos esforzaremos por responder en la máxima brevedad posible",
+						title: "Excelente!",
+						text: "Su correo ha sido enviado con éxito, nos esforzaremos por responder en la máxima brevedad posible",
 						type: "success",
-						timer:7000
+						timer: 7000
 					});
 
-					$('.form-control', $form).val("").removeClass('active valid');
-					$('label', $form).removeClass('active valid');
-					$('#send', $form).prop('disabled', true);
+					$elements.val("").removeClass("active valid");
+					$('label', form).removeClass("active valid");
+					$(BTN_SEND, form).prop('disabled', true);
 				},
-				error: function(){
+				error: function () {
 					swal({
-						title:"Error!",
-						text:"Algo salió mal, por favor verifique su conexión de internet",
-						type:"error",
-						timer:7000
+						title: "Error!",
+						text: "Algo salió mal, por favor verifique su conexión de internet",
+						type: "error",
+						timer: 7000
 					});
 				}
 			});
@@ -267,92 +266,90 @@
 		return false;
 	}
 	
-	function contactUsInSm(){
-        if(window.innerWidth >= 768 && window.innerWidth < 992){
+	function contactUsInSm() {
+        if (window.innerWidth >= 768 && window.innerWidth < 992) {
             $('.contact-method .form-control:eq(0)').attr('placeholder', "Nombre");
             $('.contact-method .form-control:eq(1)').attr('placeholder', "Correo");
             $('.contact-method .form-control:eq(2)').attr('placeholder', "Mensaje");
             $('.contact-method ul.contact').addClass("row");
             $('.contact-method ul.contact li').addClass("col-sm-3");
-        }else{
+        } else {
             $('.contact-method .form-control').attr('placeholder', "");
             $('.contact-method ul.contact').removeClass("row");
             $('.contact-method ul.contact li').removeClass();
         }
     }
 
-	function templateXs(){
-		var $form = '#contact-form';
-		var $elements = '.form-control';
-		var $classes = {
-			focusOut: "contact-form",
-			focusIn: "contact-form-focus",
-			hide: "hidden"
-		}
-
+	function templateXs() {
 		/*-- fullpage instance & config --*/
 		$(this).fullpage({
-			//
 			sectionsColor: ['#1e6f63', '#fff', '#FFA042', '#f5f5f5', '#2e3031'],
 			scrollingSpeed: 1000,
 			navigation: true,
 			navigationPosition: 'right',
 			continuousVertical: false,
 			slidesNavigation: true,
-			controlArrows: false,
+			controlArrows: false
 		});
 
 		/*-- flipCarousel instance XS & config --*/
 		$('.flip-img').flipcarousel.destroy();
 		$('.flip-img-xs').flipcarousel({
-			itemsperpage: 1,
+			itemsperpage: 1
 		});
 
 		/*-- to disabled #send button --*/
-		$('#send', $form).prop('disabled', true);
+		$(BTN_SEND, FORM_XS).prop('disabled', true);
 
 		/*-- to active form --*/
-		$($elements , $form).focusin(function(){
-			$($form)
-				.removeClass($classes.focusOut)
-				.addClass($classes.focusIn)
-				.find('.hidden')
-					.removeClass($classes.hide);
+		$(FORM_CONTROL, FORM_XS).focusin(function () {
+			$(FORM_XS)
+				.removeClass(FORM_FOCUSIN)
+				.addClass(FORM_FOCUSOUT)
+				.find('.' + FORM_HIDE)
+					.removeClass(FORM_HIDE);
 
 			$.fn.fullpage.setAllowScrolling(false);
 		});
 
 		/*-- to unactive form --*/
-		$($elements, $form).focusout(onFocusOut);
+		$(FORM_CONTROL, FORM_XS).focusout(onFocusOut);
 
 		/*-- to validate form --*/
-		$('.form-control', $form).on('keyup change', validateInput);
+		$(FORM_CONTROL, FORM_XS).on('keyup change', validateInput);
 
 		/*-- send form --*/
-		$('#send', $form).touchend(sendEmail);
+		$(BTN_SEND, FORM_XS).touchend(sendEmail);
 
 		/*-- unactive form function --*/
-		function onFocusOut(){
-			$($form)
-				.removeClass($classes.focusIn)
-				.addClass($classes.focusOut)
-				.find('#send')
-					.addClass($classes.hide)
+		function onFocusOut() {
+			$(FORM_XS)
+				.removeClass(FORM_FOCUSIN)
+				.addClass(FORM_FOCUSOUT)
+				.find(BTN_SEND)
+					.addClass(FORM_HIDE)
 				.siblings('#back')
-					.addClass($classes.hide);
+					.addClass(FORM_HIDE);
 
 			$.fn.fullpage.setAllowScrolling(true);
 
 			return false;
 		}
+
+		/*-- move to slide --*/
+		$('li', '#menu-xs').click(function () {
+			var index = $(this).index() + 2;
+
+			setTimeout(function () { $.fn.fullpage.moveTo(index); }, 200);
+		});
 	}
 
-	function viewXs(){
-		if( window.innerWidth < 768 && !$('body').attr('class') ){
+	function viewXs() {
+		if (window.innerWidth < 768 && !$('body').attr('class')) {
 
 			$('#fullpage').load("xs.html", templateXs);
 
-		} else if( window.innerWidth >= 768 && $('body').attr('class') ) {
+		} else if (window.innerWidth >= 768 && $('body').attr('class')) {
 			$('#fullpage').children().detach();
 			$.fn.fullpage.destroy("all");
 			$('body').removeAttr('class');
@@ -360,7 +357,7 @@
 			$('.flip-img-xs').flipcarousel.destroy();
 			$('.flip-img').flipcarousel();
 
-		} else if( window.innerWidth < 768 && $('body').attr('class') ){
+		} else if (window.innerWidth < 768 && $('body').attr('class')) {
 			$.fn.fullpage.reBuild();
 		}
 	}
