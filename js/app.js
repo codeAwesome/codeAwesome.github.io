c62af609276e398030748f782411a8f2b88aa18e
Otniel <otniel.a.55@gmail.com>
2016-11-07 17:04:45 -0400

    reestructuracion de la pagina en md y sm

--- js/app.js
+++ js/app.js
@@ -333,154 +333,6 @@
 		return false;
 	}
 
-	/**
-	 * activate the usage instuctions for mobile devices.
-	 */
-	function runIntructions() {
-		var $nav = $('#fp-nav'),
-        	$buttonIgnoreSeccond = $('.button-ingore-instruccions:eq(1)'),
-            $description = $('.instruccions h2'),
-            $divAnimation = $('.animation'),
-            $elementsAnimated = $('.animation, .instruccions h2');
-
-		/** creation of cookie **/
-        Cookies.set('instruccions', true);
-
-        $('instruccions').show();
-        $nav.hide();
-        $('#menu-xs').addClass(FORM_HIDE);
-
-        /** event to close the instructions **/
-        $('.close-instruccions, .button-ingore-instruccions:eq(0)').touchend(function () {
-            setTimeout(function () {
-                $('.instruccions').velocity('transition.fadeOut', {complete : function () {
-                    $(this).css('display', 'none');
-                }});
-                $('#menu-xs').removeClass(FORM_HIDE);
-                $nav.show();
-                /*-- superslides --*/
-		        $('#slides').superslides({'play': 15000});
-            }, 200);
-        });
-        
-        
-        /** change instruction **/
-        $buttonIgnoreSeccond.touchend(function () {
-            setTimeout(function () {
-                if (!$divAnimation.hasClass(HORIZONTAL_CLASS)) {
-                    $buttonIgnoreSeccond.html('Anterior');
-                    $elementsAnimated
-                        .velocity('transition.fadeOut', {complete : function () {
-                            $divAnimation.addClass(HORIZONTAL_CLASS);
-                            $description.html("Delice horizontalmente para moverse dentro de una sección");
-                        }})
-                        .velocity('transition.fadeIn');
-                } else {
-                    $buttonIgnoreSeccond.html('Siguiente');
-                    $elementsAnimated
-                        .velocity('transition.fadeOut', {complete : function () {
-                            $divAnimation.removeClass(HORIZONTAL_CLASS);
-                            $description.html("Deslice verticalmente para cambiar de sección");
-                        }})
-                        .velocity('transition.fadeIn');
-                }
-            }, 200);
-        });
-	}
-
-    /**
-     * short the text excess of the long paragraphs and create a new slide with the text cutted.
-     * @param {object} container - container parent of <p> which determines the maximum high of paragraph.
-     */
-    function createEllipsis(container) {
-        var $container = $(container),
-            $text = "",
-            containerHeight = 0,
-			text = "",
-			newText = "",
-            regex = new RegExp("[ ]+[.]");
-
-        /**
-         * [[Description]]
-         * @param   {[[Type]]} $parent [[Description]]
-         * @returns {[[Type]]} [[Description]]
-         */
-        function replaceText($parent) {
-			var textHidden = "";
-
-            if ($text.length > 0) {
-                //replace any space with a "." for "."
-				text = $text.text().replace(regex, ".");
-
-                while ($text[0].scrollHeight > containerHeight) {
-                    $text.text(function (index, text) {
-                        return text.replace(/\W*\s(\S)*$/, "...");
-                    });
-                }
-
-				newText = $text.text();
-
-				if (text.length > newText.length) {
-					text = text.substring(newText.length - 3);
-                    //verify that text[0] start with "." or space
-                    if (text[0] === "." || text[0] === " ") {
-                        //remove any "." or space that is in the first two letters of text
-                        textHidden = text.substring(0, 2).replace(/[. ]/g, "");
-                        //verify that textHidden is empty,if so is because the first two letters of text are "." or space
-                        if (textHidden.length === 0) {
-                            //textHidden is equal to the first two letters of text
-                            textHidden = text.substring(0, 2);
-                            //text is equal to the same without the first two letters
-                            text = text.substring(2);
-                        } else {
-                            //textHidden is equal to the first letters of text
-                            textHidden = text.substring(0, 1);
-                            //text is equal to the same without the first letters
-                            text = text.substring(1);
-                        }
-                    }
-
-					$parent.parent().parent().parent().after(
-						'<div class="slide">\
-							<div class="container">\
-								<div class="xs-body">\
-									<p class="next-text"> <span class="hidden">' + textHidden + '</span>' + text + '</p>\
-								</div>\
-							</div>\
-						</div>'
-					);
-				}
-            }
-        }
-
-		$.each($('.container > .xs-body:first-child'), function (k, element) {
-			var content = element.childNodes[1].textContent,
-				removeNode = element.parentNode.parentNode.parentNode,
-				sibling = removeNode.previousSibling;
-
-			$(removeNode).detach();
-			$(sibling).children().children().children('.xs-body').children().text(function (i, text) {
-                //replace any space with a "." for "."
-				text = text.substr(0, text.length - 3).replace(regex, ".");
-				return text + content;
-			});
-		});
-
-        if ($container.length > 1) {
-            $container.each(function () {
-                var $this = $(this);
-                containerHeight = $this.height();
-                $text = $this.find("p");
-                replaceText($this);
-            });
-        } else {
-            containerHeight = $container.height();
-            $text = $container.find("p");
-            replaceText($container);
-        }
-
-    }
-
 /*--------- template controllers  ----------*/
 	/**
 	 * mobile controller.
@@ -488,27 +340,8 @@
 //	function mobileCtrl() {
 //
 //	/*---------- header controller ----------*/
-//		$('#fp-nav').hide();
-//
-//		/*-- execute intructions --*/
-//        if (!Cookies.get('instruccions')) {
-//            $('.instruccions').show();
-//            runIntructions();
-//        } else {
-//            $superslides.superslides({'play': 15000});
-//        }
-//
-//		/*-- move page toward the section selected --*/
-//		$('li', '#menu-xs').click(function () {
-//			var index = $(this).index() + 2;
-//
-//			setTimeout(function () { $.fn.fullpage.moveTo(index); }, 200);
-//		});
 //
 //	/*--------- services controller ---------*/
-//
-//		/*-- create flipCarousel instance --*/
-//		$('.flip-img-xs').flipcarousel({itemsperpage: 1});
 //
 //	/*--------- contactUS controller --------*/
 //
@@ -552,23 +385,41 @@
 	function tabAndDesktopCtrl() {
 
 		/*-- swiper carousel instance --*/
-		var mySwiper = new Swiper('.swiper-container', {
-			autoHeight: true,
-			loop: true,
-			speed: 800,
-			autoplay: 5000,
-			paginationClickable: true,
-			lazyLoading: true,
-			pagination: '.swiper-pagination',
-			nextButton: '.swiper-button-next',
-			prevButton: '.swiper-button-prev',
-			effect: 'coverflow',
-			grabCursor: true,
-			centeredSlides: true,
-			slidesPerView: 'auto',
-		});
+		var mySwiper =  new Swiper('.swiper-main', {
+				autoHeight: true,
+				loop: true,
+				speed: 800,
+				autoplay: 5000,
+				paginationClickable: true,
+				lazyLoading: true,
+				pagination: '.swiper-pagination',
+				nextButton: '.swiper-button-next',
+				prevButton: '.swiper-button-prev',
+				effect: 'coverflow',
+				grabCursor: true,
+				centeredSlides: true,
+				slidesPerView: 'auto',
+			}),
+			swiperLang  = new Swiper('.swiper-lang-container', {
+				loop: true,
+				speed: 500,
+				autoplay: 1000,
+				pagination: '.swiper-pagination',
+				mousewheelControl: true,
+				effect: 'coverflow',
+				grabCursor: true,
+				centeredSlides: true,
+				slidesPerView: 'auto',
+				coverflow: {
+					rotate: 50,
+					stretch: 0,
+					depth: 100,
+					modifier: 1,
+					slideShadows : false
+				}
+			});
 
-		$('.swiper-wrapper').height($('html').width() * 0.4);
+		$('.swiper-wrapper-main').height($('html').width() * 0.4);
 
     	/*-- hyphenate the text of all <p> --*/
 		$('p').hyphenate('es');
@@ -587,11 +438,6 @@
 
 		/*-- move page toward the section selected --*/
 		$('a[href*="#"]').click(slideScrollAnimation);
-
-	/*--------- services controller ---------*/
-
-		/*-- create flipCarousel instance --*/
-		$('.flip-img').flipcarousel();
 
 	/*--------- contactUS controller --------*/
 
@@ -645,6 +491,8 @@
 	});
 
 	$(window).resize(function () {
+		var vw = window.innerWidth;
+
 		activateSmForm(vw);
 		alignImagesSm(vw);
         alignSocialIcons();
